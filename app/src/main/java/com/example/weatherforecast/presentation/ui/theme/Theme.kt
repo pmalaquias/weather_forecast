969368c39a5cc4e64942268bbfb03daf9b0d61aa
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2025-05-23 16:28:02 -0300

    Add color and typography themes for the Weather Forecast app

--- app/src/main/java/com/example/weatherforecast/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/example/weatherforecast/presentation/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
-package com.example.weatherforecast.ui.theme
+package com.example.weatherforecast.presentation.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
