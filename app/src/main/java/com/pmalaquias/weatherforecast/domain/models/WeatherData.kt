0d66a65c50f1bdf276c22c5450f6bcd598b35486
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2025-05-24 14:22:59 -0300

    Add WeatherData data class for encapsulating weather information

---
+++ app/src/main/java/com/pmalaquias/weatherforecast/domain/models/WeatherData.kt
@@ -0,0 +1,12 @@
+package com.pmalaquias.weatherforecast.domain.models
+
+/**
+ * Represents the weather data for a specific location.
+ *
+ * @property location Information about the location for which the weather data is provided.
+ * @property current The current weather conditions at the specified location.
+ */
+data class WeatherData(
+    val location: LocationInfo,
+    val current: CurrentWeather
+)
