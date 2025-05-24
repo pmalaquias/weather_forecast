3dfe69969c8ba582cb21da83f8114657a6655108
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2025-05-24 14:21:18 -0300

    Add LocationInfo data class for representing location details

---
+++ app/src/main/java/com/pmalaquias/weatherforecast/domain/models/LocationInfo.kt
@@ -0,0 +1,16 @@
+package com.pmalaquias.weatherforecast.domain.models
+
+/**
+ * Represents information about a specific location.
+ *
+ * @property name The name of the location (e.g., city or town).
+ * @property region The region or state where the location is situated.
+ * @property country The country of the location.
+ * @property localtime The local time at the location, typically in a formatted string.
+ */
+data class LocationInfo(
+    val name: String,
+    val region: String,
+    val country: String,
+    val localtime: String
+)
