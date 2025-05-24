0c0c954081311608fe79beb1921e29c850f39d0a
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2025-05-24 14:29:26 -0300

    Add CurrentWeather data class for representing current weather conditions

---
+++ app/src/main/java/com/pmalaquias/weatherforecast/domain/models/CurrentWeather.kt
@@ -0,0 +1,20 @@
+package com.pmalaquias.weatherforecast.domain.models
+
+/**
+ * Represents the current weather conditions.
+ *
+ * @property tempCelcius The current temperature in Celsius.
+ * @property condition The current weather condition.
+ * @property windKph The wind speed in kilometers per hour.
+ * @property humidity The current humidity percentage.
+ * @property feelslikeCelcius The perceived temperature in Celsius.
+ * @property isDay Indicator if it is day (1) or night (0).
+ */
+data class CurrentWeather(
+    val tempCelcius: Double,
+    val condition: WeatherCondition,
+    val windKph: Double,
+    val humidity: Int,
+    val feelslikeCelcius: Double,
+    val isDay: Int
+)
