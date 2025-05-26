f6d7dbe7fce15659f38f6f3aa519d30455540589
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2025-05-26 19:30:58 -0300

    Add Coil dependency for image loading in Compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.coil.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
