e64ac8b3902165426e546231ffd4c8e1518cac58
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2025-05-28 17:16:04 -0300

    Add WeatherDataDisplay composable with preview data and additional weather details

--- app/src/main/java/com/pmalaquias/weatherforecast/presentation/ui/pages/weatherScreen/WeatherDataDisplay.kt
+++ app/src/main/java/com/pmalaquias/weatherforecast/presentation/ui/pages/weatherScreen/WeatherDataDisplay.kt
@@ -11,10 +11,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
+import com.pmalaquias.weatherforecast.domain.models.CurrentWeather
+import com.pmalaquias.weatherforecast.domain.models.LocationInfo
+import com.pmalaquias.weatherforecast.domain.models.WeatherCondition
 import com.pmalaquias.weatherforecast.domain.models.WeatherData
+import com.pmalaquias.weatherforecast.presentation.ui.theme.AppTheme
 
 /**
  * Displays weather data in a vertically arranged column.
@@ -54,4 +59,42 @@ fun WeatherDataDisplay(weatherData: WeatherData) {
         Text(text = "Umidade: ${weatherData.current.humidity}%")
         Text(text = "Vento: ${weatherData.current.windKph} km/h")
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WeatherDataDisplayPreview(){
+    AppTheme {
+        WeatherDataDisplay(
+            weatherData = PreviewData.sampleWeatherData
+        )
+    }
+}
+
+object PreviewData {
+    val sampleWeatherData = WeatherData(
+        location = LocationInfo(
+            name = "Ouro Preto",
+            region = "Minas Gerais",
+            country = "Brasil",
+            localtime = "2025-05-26 20:00"
+        ),
+        current = CurrentWeather(
+            tempCelcius = 22.5,
+            condition = WeatherCondition(
+                text = "Parcialmente Nublado",
+                iconUrl = "https://cdn.weatherapi.com/weather/64x64/day/116.png" 
+            ),
+            windKph = 10.2,
+            humidity = 65,
+            feelslikeCelcius = 21.8,
+            isDay = 1
+        )
+    )
+
+    val loadingState = WeatherUIState(isLoading = true)
+
+    val errorState = WeatherUIState(errorMessage = "Falha ao carregar dados (Preview)")
+
+    val successState = WeatherUIState(weatherData = sampleWeatherData)
 }
