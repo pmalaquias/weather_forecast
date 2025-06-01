d6962de38323823df9565d75be40eb19745334f4
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2025-06-01 15:53:52 -0300

    Refactor WeatherDataDisplay to use Scaffold and improve layout; add wind direction, UV index, pressure, and precipitation to current weather display

--- app/src/main/java/com/pmalaquias/weatherforecast/presentation/ui/pages/weatherScreen/WeatherDataDisplay.kt
+++ app/src/main/java/com/pmalaquias/weatherforecast/presentation/ui/pages/weatherScreen/WeatherDataDisplay.kt
@@ -1,15 +1,35 @@
 package com.pmalaquias.weatherforecast.presentation.ui.pages.weatherScreen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.Colors
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -34,36 +54,88 @@ import com.pmalaquias.weatherforecast.presentation.ui.theme.AppTheme
  * - The humidity percentage.
  * - The wind speed in km/h.
  */
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun WeatherDataDisplay(weatherData: WeatherData) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-        modifier = Modifier.padding(16.dp)
-    ) {
-        Text(text = "Local: ${weatherData.location.name}, ${weatherData.location.country}", fontSize = 20.sp)
-        Spacer(modifier = Modifier.height(8.dp))
-        Text(text = "${weatherData.current.tempCelcius}°C", fontSize = 48.sp, fontWeight = FontWeight.Bold)
-        Spacer(modifier = Modifier.height(8.dp))
-        Text(text = weatherData.current.condition.text, fontSize = 18.sp)
+
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
+
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            TopAppBar(
+                title = { Text(text = weatherData.location.name) },
+                navigationIcon = {
+                    IconButton(onClick = { /* Handle back navigation */ }) {
+                        Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
+                    }
+                },
+                scrollBehavior = scrollBehavior,
+
+                )
+        },
+        content = { innerPadding ->
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .fillMaxSize()
+                    .fillMaxHeight()
+            ) {
+                Box (
+                    modifier = Modifier.fillMaxWidth()
+                ){
+                    Text(
+                        text = weatherData.current.condition.text,
+                        fontSize = 24.sp,
+                        modifier = Modifier
+                            .align(Alignment.Center)
+                            .padding(8.dp),
+                    )
+                }
+                /*Text(
+                    text = "Local: ${weatherData.location.name}, ${weatherData.location.country}",
+                    fontSize = 20.sp
+                )*/
+                // Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = "${weatherData.current.tempCelcius}°C",
+                    fontSize = 64.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier = Modifier.height(8.dp))
 
 
-        AsyncImage(
-            model = weatherData.current.condition.iconUrl,
-            contentDescription = weatherData.current.condition.text,
-            modifier = Modifier.size(64.dp)
-        )
+                AsyncImage(
+                    model = weatherData.current.condition.iconUrl,
+                    contentDescription = weatherData.current.condition.text,
+                    modifier = Modifier.size(64.dp)
+                )
 
-        Spacer(modifier = Modifier.height(8.dp))
-        Text(text = "Sensação: ${weatherData.current.feelslikeCelcius}°C")
-        Text(text = "Umidade: ${weatherData.current.humidity}%")
-        Text(text = "Vento: ${weatherData.current.windKph} km/h")
-    }
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(text = "Sensação: ${weatherData.current.feelslikeCelcius}°C")
+
+                /*Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp),
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                ) {
+                    Text(text = "Máxima: ")
+                    Text(text = "Mínima:")
+                }*/
+                Text(text = "Umidade: ${weatherData.current.humidity}%")
+                Text(text = "Vento: ${weatherData.current.windKph} km/h")
+            }
+        }
+    )
 }
+
 
 @Preview(showBackground = true)
 @Composable
-fun WeatherDataDisplayPreview(){
+fun WeatherDataDisplayPreview() {
     AppTheme {
         WeatherDataDisplay(
             weatherData = PreviewData.sampleWeatherData
@@ -77,18 +149,26 @@ object PreviewData {
             name = "Ouro Preto",
             region = "Minas Gerais",
             country = "Brasil",
-            localtime = "2025-05-26 20:00"
+            localtime = "2025-05-26 20:00",
+            timezoneId = "America/Sao_Paulo",
+            lat = -20.3833,
+            lon = -43.5033
         ),
         current = CurrentWeather(
             tempCelcius = 22.5,
             condition = WeatherCondition(
                 text = "Parcialmente Nublado",
-                iconUrl = "https://cdn.weatherapi.com/weather/64x64/day/116.png"
+                iconUrl = "https://cdn.weatherapi.com/weather/64x64/day/116.png",
+                code = 116
             ),
             windKph = 10.2,
             humidity = 65,
             feelslikeCelcius = 21.8,
-            isDay = 1
+            isDay = 1,
+            windDir = "Noroeste",
+            uv = 5,
+            pressureMb = 1012.0,
+            precipitationMm = 0.0
         )
     )
 
