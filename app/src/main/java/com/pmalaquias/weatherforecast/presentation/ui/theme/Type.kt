1d62e6f4d546692acfab0efc6794e8e891a78a13
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2025-06-08 18:38:47 -0300

    Update Typography to use Roboto Flex variable font for enhanced text styling

--- app/src/main/java/com/pmalaquias/weatherforecast/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/pmalaquias/weatherforecast/presentation/ui/theme/Type.kt
@@ -2,14 +2,20 @@ package com.pmalaquias.weatherforecast.presentation.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.pmalaquias.weatherforecast.R
+
+val RobotoFlex = FontFamily(
+    Font(R.font.roboto_flex_variable_font),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = RobotoFlex,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
