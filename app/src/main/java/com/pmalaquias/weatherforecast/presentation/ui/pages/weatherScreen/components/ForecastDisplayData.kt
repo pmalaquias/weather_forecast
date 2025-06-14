7d86201fa1d5c85e45fd1f08591e956b5df2890d
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2025-06-14 17:31:47 -0300

    Refactor ForecastDisplayData to add text color parameter and improve DailyForecastItem styling

--- app/src/main/java/com/pmalaquias/weatherforecast/presentation/ui/pages/weatherScreen/components/ForecastDisplayData.kt
+++ app/src/main/java/com/pmalaquias/weatherforecast/presentation/ui/pages/weatherScreen/components/ForecastDisplayData.kt
@@ -1,29 +1,19 @@
 package com.pmalaquias.weatherforecast.presentation.ui.pages.weatherScreen.components
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
-import com.pmalaquias.weatherforecast.domain.models.DailyForecast
 import com.pmalaquias.weatherforecast.domain.models.ForecastData
 import com.pmalaquias.weatherforecast.presentation.ui.pages.weatherScreen.PreviewData
-import com.pmalaquias.weatherforecast.presentation.ui.utils.DateUtils
-import java.util.Locale
 
 @Composable
 fun ForecastDisplayData(
     forecastData: ForecastData?,
+    textColor: Color = Color.Black,
 ) {
 
     if (forecastData == null || forecastData.dailyForecasts.isEmpty()) {
@@ -37,9 +27,11 @@ fun ForecastDisplayData(
         items(
             count = forecastData.dailyForecasts.size,
             key = { index -> forecastData.dailyForecasts[index].date }) { index ->
-            DailyForecastItem(dailyForecast = forecastData.dailyForecasts[index])
+            DailyForecastItem(
+                dailyForecast = forecastData.dailyForecasts[index],
+                textColor = textColor
+            )
         }
-
     }
 }
 
@@ -48,6 +40,6 @@ fun ForecastDisplayData(
 @Composable
 fun ForecastDisplayDataPreview() {
     ForecastDisplayData(
-        forecastData = PreviewData.sampleForecastData
+        forecastData = PreviewData.sampleForecastData,
     )
 }
