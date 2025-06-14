de092c9cc34e2d3f1df554af27a8262eb2b2ad23
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2025-06-14 17:30:41 -0300

    Refactor HumidityDataDisplay to add text color parameter, enhance text styling, and improve progress indicator dimensions

--- app/src/main/java/com/pmalaquias/weatherforecast/presentation/ui/pages/weatherScreen/components/HumidityDataDisplay.kt
+++ app/src/main/java/com/pmalaquias/weatherforecast/presentation/ui/pages/weatherScreen/components/HumidityDataDisplay.kt
@@ -8,25 +8,40 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.LinearWavyProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.pmalaquias.weatherforecast.presentation.ui.theme.daySunnyColor1
 
 @OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun HumidityDataDisplay(
-    humidityData: Int
+    humidityData: Int, textColor: Color = Color.Black
 ) {
 
-    Card(){
+    val thickStrokeWidth = with(LocalDensity.current) { 10.dp.toPx() }
+    val thickStroke =
+        remember(thickStrokeWidth) { Stroke(width = thickStrokeWidth, cap = StrokeCap.Round) }
+
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White.copy(alpha = 0.4f),
+        )
+    ) {
         Column(
             modifier = Modifier
                 .height(100.dp)
@@ -41,7 +56,11 @@ fun HumidityDataDisplay(
             ) {
                 Text("💧", fontSize = 20.sp)
                 Spacer(Modifier.width(4.dp))
-                Text("Umidade", style = MaterialTheme.typography.titleSmallEmphasized)
+                Text(
+                    "Umidade",
+                    style = MaterialTheme.typography.titleSmallEmphasized,
+                    color = textColor
+                )
             }
 
             Spacer(modifier = Modifier.height(16.dp))
@@ -53,13 +72,19 @@ fun HumidityDataDisplay(
                     "$humidityData %",
                     style = MaterialTheme.typography.bodyLarge,
                     fontWeight = FontWeight.Bold,
-                    modifier = Modifier.padding(horizontal = 8.dp)
+                    modifier = Modifier.padding(horizontal = 8.dp),
+                    color = textColor
                 )
                 LinearWavyProgressIndicator(
                     progress = { humidityData / 100f },
                     modifier = Modifier
                         .padding(top = 8.dp)
-                        .height(8.dp),
+                        .height(8.dp)
+                        .width(300.dp),
+                    color = daySunnyColor1,
+                    trackColor = Color.LightGray,
+                    stroke = thickStroke,
+                    trackStroke = thickStroke,
                 )
             }
         }
