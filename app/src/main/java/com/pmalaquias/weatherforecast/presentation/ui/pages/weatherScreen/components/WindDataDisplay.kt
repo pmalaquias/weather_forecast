68ff021d39cd7b1cb62c68ed4ebeba5436732ce3
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2025-06-14 17:28:03 -0300

    Refactor WindDataDisplay to add text color parameter, enhance layout, and improve styling for wind information

--- app/src/main/java/com/pmalaquias/weatherforecast/presentation/ui/pages/weatherScreen/components/WindDataDisplay.kt
+++ app/src/main/java/com/pmalaquias/weatherforecast/presentation/ui/pages/weatherScreen/components/WindDataDisplay.kt
@@ -6,17 +6,19 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -26,13 +28,18 @@ import com.pmalaquias.weatherforecast.domain.models.WindInfo
 @OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun WindDataDisplay(
-    windInfo: WindInfo
+    windInfo: WindInfo, textColor: Color = Color.Black
 ) {
-    Card {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White.copy(alpha = 0.4f),
+        )
+    ) {
         Column(
             modifier = Modifier
                 .width(150.dp)
                 .height(100.dp)
+                .fillMaxWidth()
                 .padding(8.dp),
             horizontalAlignment = Alignment.Start,
             verticalArrangement = Arrangement.Center
@@ -44,13 +51,18 @@ fun WindDataDisplay(
             ) {
                 Text("🍃", fontSize = 20.sp)
                 Spacer(Modifier.width(4.dp))
-                Text("Vento", style = MaterialTheme.typography.titleSmallEmphasized)
+                Text(
+                    "Vento",
+                    style = MaterialTheme.typography.titleSmallEmphasized,
+                    color = textColor
+                )
             }
 
             Box(
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(bottom = 8.dp),
+                //.padding(bottom = 8.dp)
+                ,
                 contentAlignment = Alignment.Center
             ) {
                 WindDirectionDisplay(
@@ -65,11 +77,13 @@ fun WindDataDisplay(
                     // Display wind speed and direction
                     Text(
                         text = "${windInfo.speed} km/h",
-                        style = MaterialTheme.typography.bodyLargeEmphasized
+                        style = MaterialTheme.typography.bodyLargeEmphasized,
+                        color = textColor
                     )
                     Text(
                         text = windInfo.direction,
-                        style = MaterialTheme.typography.bodySmallEmphasized
+                        style = MaterialTheme.typography.bodySmallEmphasized,
+                        color = textColor
                     )
                 }
             }
